c467ce957fb9f3795062cf26a22e603993becd08
vanzway <vanzway@gmail.com>
2025-01-02 10:18:02 +0200

    Update test.py

--- test.py
+++ test.py
@@ -6,10 +6,10 @@ async def query_server():
 	uri = "ws://localhost:8765"
 	async with websockets.connect(uri, ping_interval=30, ping_timeout=600) as websocket:
 		message = {
-			"query": "What are the main points in the articles?",
+			"query": "Tell me about this product",
 			"urls": [
-				"https://www.thehindu.com/news/international/south-korea-plane-crash-jeju-aircraft-muan-airport-live-updates-december-29/article69039217.ece",
-				"https://www.thehindu.com/news/international/azerbaijan-accuses-russia-of-trying-to-hide-causes-of-plane-crash-says-the-plane-was-shot-from-russia/article69040088.ece"
+				"https://artcode.co.za/mmxxv",
+				"https://github.com/vanzway/mmxxv"
 			]
 		}
 		await websocket.send(json.dumps(message))
